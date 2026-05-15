7a8d2d31fc4ee949d4f4e13a93d68b621e6b7290
ronaksolanki98 <solankironak423@gmail.com>
2026-05-15 20:19:41 +0530

    Fixed Key issue

--- infrastructure/terraform/modules/blob_storage/main.tf
+++ infrastructure/terraform/modules/blob_storage/main.tf
@@ -10,18 +10,26 @@ terraform {
 }
 
 resource "azurerm_storage_account" "this" {
-  name                            = var.storage_account_name
-  resource_group_name             = var.resource_group_name
-  location                        = var.location
-  account_tier                    = var.account_tier
-  account_replication_type        = var.replication_type
-  access_tier                     = var.access_tier
-  min_tls_version                 = "TLS1_2"
-  allow_nested_items_to_be_public = var.allow_blob_public_access
-  tags                            = var.tags
+  name                              = var.storage_account_name
+  resource_group_name               = var.resource_group_name
+  location                          = var.location
+  account_tier                      = var.account_tier
+  account_replication_type          = var.replication_type
+  access_tier                       = var.access_tier
+  min_tls_version                   = "TLS1_2"
+  allow_nested_items_to_be_public   = var.allow_blob_public_access
+  shared_access_key_enabled         = false
+  infrastructure_encryption_enabled = true
+  public_network_access_enabled     = false
+  tags                              = var.tags
 
   blob_properties {
     versioning_enabled = var.enable_versioning
+  }
+
+  network_rules {
+    default_action = "Deny"
+    bypass         = ["AzureServices"]
   }
 }
 
--- infrastructure/terraform/modules/key_vault/main.tf
+++ infrastructure/terraform/modules/key_vault/main.tf
@@ -10,15 +10,16 @@ terraform {
 }
 
 resource "azurerm_key_vault" "kv" {
-  name                        = var.name
-  location                    = var.location
-  resource_group_name         = var.resource_group_name
-  tenant_id                   = var.tenant_id
-  sku_name                    = "standard"
-  purge_protection_enabled    = false
-  soft_delete_retention_days  = 7
-  enabled_for_disk_encryption = true
-  tags                        = var.tags
+  name                          = var.name
+  location                      = var.location
+  resource_group_name           = var.resource_group_name
+  tenant_id                     = var.tenant_id
+  sku_name                      = "standard"
+  purge_protection_enabled      = true
+  soft_delete_retention_days    = 90
+  enabled_for_disk_encryption   = true
+  public_network_access_enabled = false
+  tags                          = var.tags
 
   access_policy {
     tenant_id = var.tenant_id
